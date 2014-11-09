a72400982d0940829fb29c2d44cb0b3b622bd8b1
zzzzizu <zzzzizu@gmail.com>
2014-11-09 19:37:44 +0200

    homework

--- src/il/ac/mta/Stock_applicationServlet.java
+++ src/il/ac/mta/Stock_applicationServlet.java
@@ -7,7 +7,16 @@ import javax.servlet.http.*;
 public class Stock_applicationServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
+		resp.setContentType("text/html");
+		int num1; 
+		int num2;
+		int num3;
+		num1 = 2;
+		num2 = 5;
+		num3 = num1*num2;
+		String resultStr = new String("<h1>Result of"+num1+"*"+num2+"="+num3+"</h1>");
+		resp.getWriter().println(resultStr);
 		resp.getWriter().println("Hello, world daniel daniel");
 	}
 }
+
