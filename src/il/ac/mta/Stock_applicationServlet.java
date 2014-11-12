7cfb8899e2453d2b0840cf4039953373c581395c
zzzzizu <zzzzizu@gmail.com>
2014-11-12 15:10:46 +0200

    fixed-HW

--- src/il/ac/mta/Stock_applicationServlet.java
+++ src/il/ac/mta/Stock_applicationServlet.java
@@ -14,7 +14,7 @@ public class Stock_applicationServlet extends HttpServlet {
 		num1 = 2;
 		num2 = 5;
 		num3 = num1*num2;
-		String resultStr = new String("<h1>Result of"+num1+"*"+num2+"="+num3+"</h1>");
+		String resultStr = new String("<h1>Result of "+num1+"*"+num2+"="+num3+"</h1>");
 		resp.getWriter().println(resultStr);
 		resp.getWriter().println("Hello, world daniel daniel");
 	}
