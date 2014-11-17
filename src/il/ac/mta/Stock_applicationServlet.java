ed287e9ee884b3eaae14c7fd5183de6a541f5f88
zzzzizu <zzzzizu@gmail.com>
2014-11-17 08:28:24 +0200

    small fixes ex 3

--- src/il/ac/mta/Stock_applicationServlet.java
+++ src/il/ac/mta/Stock_applicationServlet.java
@@ -17,23 +17,23 @@ public class Stock_applicationServlet extends HttpServlet {
 		int result = (num1+num2)*num3;
 		String resultStr = new String("<h1>Result of ("+num1+"+"+num2+")*"+num3+"="+result+"</h1>");
 		resp.getWriter().println("<h1>ex1+ex2:<h1>");
+		resp.getWriter().println("<h1>Hello, my name is daniel lang<h1>");
 		resp.getWriter().println(resultStr);
-		resp.getWriter().println("Hello, my name is daniel lang");
 		
 		resp.getWriter().println("<h1>ex3:<h1>");
 		double radius = 50;
 		double area = Math.PI*(Math.pow(radius, 2));
-		String line1 = new String("Area of circle with radius " + radius +" is: " + area + " square cm.");
+		String line1 = new String("1) Area of circle with radius " + radius +" is: " + area + " square cm.");
 		
 		double angleB = 30;
 		double hypotenuse = 50;
 		double opposite = Math.sin(Math.toRadians(angleB))*hypotenuse;
-		String line2 = new String("Length of opposite where angle B is " + angleB + " degrees and Hypotenuse length is " + hypotenuse + " cm is: " + opposite+" cm");
+		String line2 = new String("2) Length of opposite where angle B is " + angleB + " degrees and Hypotenuse length is " + hypotenuse + " cm is: " + opposite+" cm");
 		
 		double base = 20;
 		double exp = 13;
 		double result2 = Math.pow(base, exp);
-		String line3 = new String("Power of " + base + " with exp of "+ exp + " is " + result2);
+		String line3 = new String("3) Power of " + base + " with exp of "+ exp + " is " + result2);
 		
 		String resultStr2 = line1 + "<br>" + line2 + "<br>" + line3;
 		resp.getWriter().println(resultStr2);
